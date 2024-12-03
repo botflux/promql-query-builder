3e85acf645ca92006023e746fb834efa82f2467c
Victor <victor.mendele68@gmail.com>
2024-12-03 21:44:09 +0100

    feat: expose an abstraction to build any prometheus function

--- src/functions.ts
+++ src/functions.ts
@@ -2,15 +2,31 @@ import {Buildable} from "./buildable";
 
 export class PrometheusFunction implements Buildable {
   constructor(
-    private readonly functionName: string
+    private readonly functionName: string,
+    private readonly args: Buildable[]
   ) {
   }
 
   build(): string {
-    return `${this.functionName}()`
+    const built = this.args.map(arg => arg.build()).join(", ")
+
+    return `${this.functionName}(${built})`
   }
 }
 
+/**
+ * Create a prometheus function using the given function name
+ * and arguments.
+ *
+ * You can use this function to build any missing function.
+ *
+ * @param functionName
+ * @param args
+ */
+export function prometheusFunction (functionName: string, args: Buildable[]): Buildable {
+  return new PrometheusFunction(functionName, args)
+}
+
 export function time() {
-  return new PrometheusFunction("time")
+  return prometheusFunction("time", [])
 }
--- src/instant-vector-functions.ts
+++ src/instant-vector-functions.ts
@@ -1,80 +1,68 @@
 import {Buildable} from "./buildable";
 import {TimeseriesSelector} from "./selector";
 import {promScalar} from "./primitives";
-
-class InstantVectorFunction implements Buildable {
-  constructor(
-    public readonly functionName: string,
-    public readonly args: Buildable[]
-  ) {
-  }
-
-  build(): string {
-    const args = this.args.map(arg => arg.build()).join(", ")
-    return `${this.functionName}(${args})`
-  }
-}
+import {prometheusFunction} from "./functions";
 
 export function abs(buildable: Buildable) {
-  return new InstantVectorFunction("abs", [buildable])
+  return prometheusFunction("abs", [buildable])
 }
 
 export function absent(buildable: Buildable) {
-  return new InstantVectorFunction("absent", [buildable])
+  return prometheusFunction("absent", [buildable])
 }
 
 export function ceil(buildable: Buildable) {
-  return new InstantVectorFunction("ceil", [buildable])
+  return prometheusFunction("ceil", [buildable])
 }
 
 export function clampMax(buildable: TimeseriesSelector, max: number) {
-  return new InstantVectorFunction("clamp_max", [buildable, promScalar(max)])
+  return prometheusFunction("clamp_max", [buildable, promScalar(max)])
 }
 
 export function clampMin(buildable: Buildable, min: number) {
-  return new InstantVectorFunction("clamp_min", [buildable, promScalar(min)])
+  return prometheusFunction("clamp_min", [buildable, promScalar(min)])
 }
 
 export function exp(buildable: Buildable) {
-  return new InstantVectorFunction("exp", [buildable])
+  return prometheusFunction("exp", [buildable])
 }
 
 export function floor(buildable: Buildable) {
-  return new InstantVectorFunction("floor", [buildable])
+  return prometheusFunction("floor", [buildable])
 }
 
 export function histogramQuantile(quantile: number, buildable: Buildable) {
-  return new InstantVectorFunction("histogram_quantile", [promScalar(quantile), buildable])
+  return prometheusFunction("histogram_quantile", [promScalar(quantile), buildable])
 }
 
 export function ln(buildable: Buildable) {
-  return new InstantVectorFunction("ln", [buildable])
+  return prometheusFunction("ln", [buildable])
 }
 
 export function log10(buildable: Buildable) {
-  return new InstantVectorFunction("log10", [buildable])
+  return prometheusFunction("log10", [buildable])
 }
 
 export function log2(buildable: Buildable) {
-  return new InstantVectorFunction("log2", [buildable])
+  return prometheusFunction("log2", [buildable])
 }
 
 export function round(buildable: Buildable, r: number) {
-  return new InstantVectorFunction("round", [buildable, promScalar(r)])
+  return prometheusFunction("round", [buildable, promScalar(r)])
 }
 
 export function scalar(buildable: Buildable) {
-  return new InstantVectorFunction("scalar", [buildable])
+  return prometheusFunction("scalar", [buildable])
 }
 
 export function sort(buildable: Buildable) {
-  return new InstantVectorFunction("sort", [buildable])
+  return prometheusFunction("sort", [buildable])
 }
 
 export function sortDesc(buildable: Buildable) {
-  return new InstantVectorFunction("sort_desc", [buildable])
+  return prometheusFunction("sort_desc", [buildable])
 }
 
 export function sqrt(buildable: Buildable) {
-  return new InstantVectorFunction("sqrt", [buildable])
+  return prometheusFunction("sqrt", [buildable])
 }
