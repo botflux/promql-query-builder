c0f22fc4d8ab306572c51ea1aa0f8122c62aaad6
Victor <victor.mendele68@gmail.com>
2024-12-15 19:56:46 +0100

    refactor: remove range function because it is the same as prometheus normal function
    This change shouldn't be a breaking change because range function was
    not exported altrough it was used in exported function signatures.

--- src/range-vector-functions.ts
+++ src/range-vector-functions.ts
@@ -1,97 +1,84 @@
 import {Range} from "./range";
 import {promScalar} from "./primitives";
 import {Buildable} from "./buildable";
+import {prometheusFunction} from "./functions";
 
-class RangeVectorFunction implements Buildable {
-  constructor(
-    public readonly functionName: string,
-    public readonly args: Buildable[],
-  ) {
-  }
-
-  build(): string {
-    const builtArgs = this.args.map(arg => arg.build()).join(", ")
-
-    return `${this.functionName}(${builtArgs})`
-  }
+export function changes(range: Range): Buildable {
+  return prometheusFunction("changes", [range])
 }
 
-export function changes(range: Range): RangeVectorFunction {
-  return new RangeVectorFunction("changes", [range])
+export function absentOverTime(range: Range): Buildable {
+  return prometheusFunction("absent_over_time", [range])
 }
 
-export function absentOverTime(range: Range): RangeVectorFunction {
-  return new RangeVectorFunction("absent_over_time", [range])
+export function delta(range: Range): Buildable {
+  return prometheusFunction("delta", [range])
 }
 
-export function delta(range: Range): RangeVectorFunction {
-  return new RangeVectorFunction("delta", [range])
+export function deriv(range: Range): Buildable {
+  return prometheusFunction("deriv", [range])
 }
 
-export function deriv(range: Range): RangeVectorFunction {
-  return new RangeVectorFunction("deriv", [range])
+export function holtWinters(range: Range, sf: number, tf: number): Buildable {
+  return prometheusFunction("holt_winters", [range, promScalar(sf), promScalar(tf)])
 }
 
-export function holtWinters(range: Range, sf: number, tf: number): RangeVectorFunction {
-  return new RangeVectorFunction("holt_winters", [range, promScalar(sf), promScalar(tf)])
+export function idelta(range: Range): Buildable {
+  return prometheusFunction("idelta", [range])
 }
 
-export function idelta(range: Range): RangeVectorFunction {
-  return new RangeVectorFunction("idelta", [range])
+export function increase(range: Range): Buildable {
+  return prometheusFunction("increase", [range])
 }
 
-export function increase(range: Range): RangeVectorFunction {
-  return new RangeVectorFunction("increase", [range])
+export function irate(range: Range): Buildable {
+  return prometheusFunction("irate", [range])
 }
 
-export function irate(range: Range): RangeVectorFunction {
-  return new RangeVectorFunction("irate", [range])
+export function predictLinear(range: Range, seconds: number): Buildable {
+  return prometheusFunction("predict_linear", [range, promScalar(seconds)])
 }
 
-export function predictLinear(range: Range, seconds: number) {
-  return new RangeVectorFunction("predict_linear", [range, promScalar(seconds)])
+export function rate(range: Range): Buildable {
+  return prometheusFunction("rate", [range])
 }
 
-export function rate(range: Range) {
-  return new RangeVectorFunction("rate", [range])
+export function resets(range: Range): Buildable {
+  return prometheusFunction("resets", [range])
 }
 
-export function resets(range: Range) {
-  return new RangeVectorFunction("resets", [range])
+export function avgOverTime(range: Range): Buildable {
+  return prometheusFunction("avg_over_time", [range])
 }
 
-export function avgOverTime(range: Range) {
-  return new RangeVectorFunction("avg_over_time", [range])
+export function minOverTime(range: Range): Buildable {
+  return prometheusFunction("min_over_time", [range])
 }
 
-export function minOverTime(range: Range) {
-  return new RangeVectorFunction("min_over_time", [range])
+export function maxOverTime(range: Range): Buildable {
+  return prometheusFunction("max_over_time", [range])
 }
 
-export function maxOverTime(range: Range) {
-  return new RangeVectorFunction("max_over_time", [range])
+export function sumOverTime(range: Range): Buildable {
+  return prometheusFunction("sum_over_time", [range])
 }
 
-export function sumOverTime(range: Range) {
-  return new RangeVectorFunction("sum_over_time", [range])
+export function countOverTime(range: Range): Buildable {
+  return prometheusFunction("count_over_time", [range])
 }
 
-export function countOverTime(range: Range) {
-  return new RangeVectorFunction("count_over_time", [range])
+export function quantileOverTime(quantile: number, range: Range): Buildable {
+  return prometheusFunction("quantile_over_time", [promScalar(quantile), range])
 }
 
-export function quantileOverTime(quantile: number, range: Range) {
-  return new RangeVectorFunction("quantile_over_time", [promScalar(quantile), range])
+export function stddevOverTime(range: Range): Buildable {
+  return prometheusFunction("stddev_over_time", [range])
 }
 
-export function stddevOverTime(range: Range) {
-  return new RangeVectorFunction("stddev_over_time", [range])
+export function stdvarOverTime(range: Range): Buildable {
+  return prometheusFunction("stdvar_over_time", [range])
 }
 
-export function stdvarOverTime(range: Range) {
-  return new RangeVectorFunction("stdvar_over_time", [range])
-}
-
-export function doubleExponentialSmoothing(range: Range, sf: number, tf: number) {
-  return new RangeVectorFunction("double_exponential_smoothing", [range, promScalar(sf), promScalar(tf)])
+export function doubleExponentialSmoothing(range: Range, sf: number, tf: number): Buildable {
+  return prometheusFunction("double_exponential_smoothing", [range, promScalar(sf), promScalar(tf)])
 }
