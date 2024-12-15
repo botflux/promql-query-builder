2cdf59ef2482bdf19522f1eb0c5ba90ddb752d65
Victor <victor.mendele68@gmail.com>
2024-12-15 20:32:26 +0100

    feat: add support for rad, deg, pi and trigonometric functions

--- src/functions.spec.ts
+++ src/functions.spec.ts
@@ -1,9 +1,70 @@
 import {describe, it} from "mocha"
 import {expect} from "chai";
-import {time} from "./functions";
+import {acos, acosh, asin, asinh, atan, atanh, cos, cosh, deg, pi, rad, sin, sinh, tan, tanh, time} from "./functions";
+import {timeseriesSelector} from "./selector";
 
 describe('time', function () {
   it('should be able to support "time"', () => {
     expect(time().build()).to.equal("time()")
+  })
+
+  it('should be able to support "acos"', ()=> {
+    expect(acos(timeseriesSelector("foo").withLabels({ bar: "baz" })).build()).to.equal('acos(foo{bar="baz"})')
+  })
+
+  it('should be able to support "acosh"', ()=> {
+    expect(acosh(timeseriesSelector("foo").withLabels({ bar: "baz" })).build()).to.equal('acosh(foo{bar="baz"})')
+  })
+
+  it('should be able to support "asin"', ()=> {
+    expect(asin(timeseriesSelector("foo").withLabels({ bar: "baz" })).build()).to.equal('asin(foo{bar="baz"})')
+  })
+
+  it('should be able to support "asinh"', ()=> {
+    expect(asinh(timeseriesSelector("foo").withLabels({ bar: "baz" })).build()).to.equal('asinh(foo{bar="baz"})')
+  })
+
+  it('should be able to support "atan"', ()=> {
+    expect(atan(timeseriesSelector("foo").withLabels({ bar: "baz" })).build()).to.equal('atan(foo{bar="baz"})')
+  })
+
+  it('should be able to support "atanh"', ()=> {
+    expect(atanh(timeseriesSelector("foo").withLabels({ bar: "baz" })).build()).to.equal('atanh(foo{bar="baz"})')
+  })
+
+  it('should be able to support "cos"', ()=> {
+    expect(cos(timeseriesSelector("foo").withLabels({ bar: "baz" })).build()).to.equal('cos(foo{bar="baz"})')
+  })
+
+  it('should be able to support "cosh"', ()=> {
+    expect(cosh(timeseriesSelector("foo").withLabels({ bar: "baz" })).build()).to.equal('cosh(foo{bar="baz"})')
+  })
+
+  it('should be able to support "sin"', ()=> {
+    expect(sin(timeseriesSelector("foo").withLabels({ bar: "baz" })).build()).to.equal('sin(foo{bar="baz"})')
+  })
+
+  it('should be able to support "sinh"', ()=> {
+    expect(sinh(timeseriesSelector("foo").withLabels({ bar: "baz" })).build()).to.equal('sinh(foo{bar="baz"})')
+  })
+
+  it('should be able to support "tan"', ()=> {
+    expect(tan(timeseriesSelector("foo").withLabels({ bar: "baz" })).build()).to.equal('tan(foo{bar="baz"})')
+  })
+
+  it('should be able to support "tanh"', ()=> {
+    expect(tanh(timeseriesSelector("foo").withLabels({ bar: "baz" })).build()).to.equal('tanh(foo{bar="baz"})')
+  })
+
+  it('should be able to support "deg"', ()=> {
+    expect(deg(timeseriesSelector("foo").withLabels({ bar: "baz" })).build()).to.equal('deg(foo{bar="baz"})')
+  })
+
+  it('should be able to support "rad"', ()=> {
+    expect(rad(timeseriesSelector("foo").withLabels({ bar: "baz" })).build()).to.equal('rad(foo{bar="baz"})')
+  })
+
+  it('should be able to support "pi"', ()=> {
+    expect(pi().build()).to.equal('pi()')
   })
 })
--- src/functions.ts
+++ src/functions.ts
@@ -35,3 +35,63 @@ export function time() {
 export function vector(scalarOrBuildable: number | Buildable) {
   return prometheusFunction("vector", [ typeof scalarOrBuildable === "number" ? promScalar(scalarOrBuildable) : scalarOrBuildable ])
 }
+
+export function acos (buildable: Buildable): Buildable {
+  return prometheusFunction("acos", [ buildable ])
+}
+
+export function acosh (buildable: Buildable): Buildable {
+  return prometheusFunction("acosh", [ buildable ])
+}
+
+export function asin (buildable: Buildable): Buildable {
+  return prometheusFunction("asin", [ buildable ])
+}
+
+export function asinh (buildable: Buildable): Buildable {
+  return prometheusFunction("asinh", [ buildable ])
+}
+
+export function atan (buildable: Buildable): Buildable {
+  return prometheusFunction("atan", [ buildable ])
+}
+
+export function atanh (buildable: Buildable): Buildable {
+  return prometheusFunction("atanh", [ buildable ])
+}
+
+export function cos (buildable: Buildable): Buildable {
+  return prometheusFunction("cos", [ buildable ])
+}
+
+export function cosh (buildable: Buildable): Buildable {
+  return prometheusFunction("cosh", [ buildable ])
+}
+
+export function sin (buildable: Buildable): Buildable {
+  return prometheusFunction("sin", [ buildable ])
+}
+
+export function sinh (buildable: Buildable): Buildable {
+  return prometheusFunction("sinh", [ buildable ])
+}
+
+export function tan (buildable: Buildable): Buildable {
+  return prometheusFunction("tan", [ buildable ])
+}
+
+export function tanh (buildable: Buildable): Buildable {
+  return prometheusFunction("tanh", [ buildable ])
+}
+
+export function deg (buildable: Buildable): Buildable {
+  return prometheusFunction("deg", [ buildable ])
+}
+
+export function rad (buildable: Buildable): Buildable {
+  return prometheusFunction("rad", [ buildable ])
+}
+
+export function pi (): Buildable {
+  return prometheusFunction("pi", [])
+}
